784fc05a8487358a5a27f719e3756eafe0ba874c
Devin Singh <drsingh2518@icloud.com>
2022-08-19 21:43:59 -0500

    add ZeroOrMore for variadic parsing

--- src/lib.zig
+++ src/lib.zig
@@ -5,6 +5,36 @@ pub fn ParseFn(comptime T: type) type {
     return fn (*[]const u8, anytype) ParseFnRet(T);
 }
 
+pub fn getChildParseFn(comptime T: type, comptime field: anytype) ParseFn(T) {
+    const Field = field.field_type;
+    const FieldInfo = @typeInfo(Field);
+    const has_parser = @hasDecl(T, field.name ++ "Parser");
+    const has_parse = if (comptime isStructOrUnion(Field))
+        @hasDecl(Field, "parse")
+    else
+        false;
+    return switch (FieldInfo) {
+        .Struct => if (has_parser)
+            @field(T, field.name ++ "Parser")
+        else if (has_parse)
+            @field(Field, "parse")
+        else
+            Parser(Field).parseRest,
+        .Int => if (has_parser)
+            @field(T, field.name ++ "Parser")
+        else
+            @compileError("Parser not found for " ++ @typeName(T) ++ "." ++ @typeName(field.field_type)),
+        else => @compileError("Unsupported parsing type: " ++ @tagName(FieldInfo)),
+    };
+}
+
+pub fn getParseFn(comptime T: type) ParseFn(T) {
+    return if (@hasDecl(T, "parse"))
+        T.parse
+    else
+        Parser(T).parseRest;
+}
+
 pub fn ParseFnRet(comptime T: type) type {
     return ParseError(T)!T;
 }
@@ -38,42 +68,21 @@ pub fn Parser(comptime T: type) type {
     return struct {
         const Error = ParseError(T);
 
-        fn parse(bytes: []const u8, extra: anytype) !T {
+        fn parse(bytes: []const u8, extra: anytype) ParseFnRet(T) {
             var b = bytes;
-            return if (@hasDecl(T, "parse"))
-                T.parse(&b, extra)
-            else
-                parseRest(&b, extra);
+            return getParseFn(T)(&b, extra);
         }
         // null means that parsing will continue for rest of the struct.
-        fn parseWhile(ret: *T, bytes: *[]const u8, extra: anytype, comptime stop: ?[]const u8) ParseError(T)!void {
+        fn parseWhile(bytes: *[]const u8, extra: anytype, comptime stop: ?[]const u8) ParseFnRet(T) {
+            var ret: T = undefined;
             inline for (Fields) |field| {
                 if (stop != null and field.name == stop.?) break;
-                const FieldInfo = @typeInfo(field.field_type);
-                const has_parser = @hasDecl(T, field.name ++ "Parser");
-                const has_parse = if (comptime isStructOrUnion(field.field_type))
-                    @hasDecl(field.field_type, "parse")
-                else
-                    false;
-                @field(ret, field.name) = switch (FieldInfo) {
-                    .Struct => if (has_parser)
-                        try @field(T, field.name ++ "Parser")(bytes, extra)
-                    else if (has_parse)
-                        try @field(field.field_type, "parse")(bytes, extra)
-                    else
-                        undefined,
-                    .Int => if (has_parser)
-                        try @field(T, field.name ++ "Parser")(bytes, extra)
-                    else
-                        undefined,
-                    else => @compileError("Unsupported parsing type: " ++ @tagName(FieldInfo)),
-                };
+                @field(ret, field.name) = try getChildParseFn(T, field)(bytes, extra);
             }
+            return ret;
         }
         fn parseRest(bytes: *[]const u8, extra: anytype) ParseFnRet(T) {
-            var ret: T = undefined;
-            try parseWhile(&ret, bytes, extra, null);
-            return ret;
+            return parseWhile(bytes, extra, null);
         }
     };
 }
@@ -96,6 +105,42 @@ pub fn eatChar(bytes: *[]const u8, char: u8) ParseFnRet(void) {
     return if (bytes.*[0] == char) {
         bytes.* = bytes.*[1..];
     } else error.UnexpectedToken;
+}
+
+pub fn ZeroOrMore(comptime T: type) type {
+    return struct {
+        const ParserError = std.mem.Allocator.Error || ParseError(T);
+        rest: []const T,
+
+        fn parse(bytes: *[]const u8, extra: anytype) ParseFnRet(@This()) {
+            assertExtraHasAllocator(extra);
+            const allocator = extra.allocator;
+            var list = std.ArrayList(T).init(allocator);
+
+            while (true) {
+                const t = getParseFn(T)(bytes, extra);
+                if (t) |ok|
+                    try list.append(ok)
+                else |_|
+                    return @This(){ .rest = list.toOwnedSlice() };
+            }
+        }
+        fn deinit(self: @This(), allocator: std.mem.Allocator) void {
+            allocator.free(self.rest);
+        }
+    };
+}
+
+pub fn assertExtraHasAllocator(extra: anytype) void {
+    const T = @TypeOf(extra);
+    if (!comptime isStruct(T)) @compileError("Extra must be a struct");
+    if (@hasField(T, "allocator")) {
+        const AllocatorTy = @TypeOf(extra.allocator);
+        if (AllocatorTy == std.mem.Allocator)
+            return
+        else
+            @compileError("Extra.allocator must be of type std.mem.Allocator found " ++ @typeName(AllocatorTy));
+    } else @compileError("Extra must have a field named allocator.");
 }
 
 fn parseInt(comptime T: type, len: usize, comptime radix: u8) ParseFn(T) {
@@ -164,6 +209,21 @@ test "basic parse - custom parse fn to properly parse" {
     const parsed = try ColorParser.parse("#2F14DF", .{});
 
     try std.testing.expectEqual(parsed, expected);
+}
+
+test "basic parse - zero or more" {
+    const a = SingleValue(u8, 'a');
+    const A = struct {
+        a: ZeroOrMore(a),
+    };
+
+    const AParser = Parser(A);
+
+    const expected = A{ .a = .{ .rest = &.{ .{}, .{}, .{} } } };
+    const parsed = try AParser.parse("aaab", .{ .allocator = std.testing.allocator });
+    defer parsed.a.deinit(std.testing.allocator);
+
+    try std.testing.expectEqualSlices(a, parsed.a.rest, expected.a.rest);
 }
 
 test "assert SingleValue zero-size" {
